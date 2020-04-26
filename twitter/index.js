b1d7871826a39bef56d1da73cd61176a958b3be6
Joseph Jin <jerkjoe61@gmail.com>
2020-04-26 15:19:42 -0700

    code review fix

--- twitter/index.js
+++ twitter/index.js
@@ -7,33 +7,34 @@ const port = process.env.PORT || 3000
 const jsonParser = (bodyParser.json())
 
 
+
+app.get('/tweets', (req, res) => {
+        
+    res.status(200).json({
+        error: false,
+        tweets: []
+    })
+})
+
+app.post('/tweet/new', jsonParser, (req, res) => {
+    
+    console.log(req.body)
+    res.status(200).json({
+        error: false,
+        msg: 'created!'
+    })
+    
+})
+
+app.delete('/tweet/:id/delete', jsonParser, (req, res) => {
+    console.log(req.params.id)
+    res.status(200).json({
+        error: false,
+        msg: `tweet ${req.params.id} is deleted!`,
+    })
+})
+
+
 app.listen(port, () => {
-    
     console.log(`server is running on port ${port}`)
-    
-    app.get('/tweets', (req, res) => {
-        
-        res.status(200).json({
-            error: false,
-            tweets: []
-        })
-    })
-    
-    app.post('/tweet/new', jsonParser, (req, res) => {
-        
-        console.log(req.body)
-        res.status(200).json({
-            error: false,
-            msg: 'created!'
-        })
-        
-    })
-    
-    app.delete('/tweet/:id/delete', jsonParser, (req, res) => {
-        console.log(req.params.id)
-        res.status(200).json({
-            error: false,
-            msg: `tweet ${req.params.id} is deleted!`,
-        })
-    })
 })
