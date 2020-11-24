c76a6087d093c46673cfe8bbde109182321ffacc
Joseph Jin <joseph@skyglobal.com>
2020-11-24 00:16:09 -0800

    update

--- src/App.js
+++ src/App.js
@@ -11,13 +11,26 @@ class App extends Component {
     this.setState({
       counter: this.state.counter + 1,
     });
-  }
+  };
+  decrement = () => {
+    if (this.state.counter < 1) {
+      return;
+    }
+    this.setState({
+      counter: this.state.counter - 1,
+    });
+  };
   render() {
     return (
       <div data-test="component-app">
-        <h1 data-test="counter-display">The counter is currently {this.state.counter}</h1>
+        <h1 data-test="counter-display">
+          The counter is currently {this.state.counter}
+        </h1>
         <button onClick={this.increment} data-test="increment-button">
           increment by 1
+        </button>
+        <button onClick={this.decrement} data-test="decrement-button">
+          decrement by 1
         </button>
       </div>
     );
