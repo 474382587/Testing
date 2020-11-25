e14b4c6b67626ec5e6109e514e0d047a2637ea41
Joseph Jin <joseph@skyglobal.com>
2020-11-24 20:57:20 -0800

    init

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,56 @@
-import { render, screen } from '@testing-library/react';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
+import EnzymeAdaptor from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdaptor() });
+
+/**
+ *
+ * @param {*} props
+ * @returns {ShallowWrapper} state
+ */
+const setup = (props = {}, state = null) => {
+  const wrapper = shallow(<App {...props} />);
+  if (state) {
+    wrapper.setState(state);
+  }
+  return wrapper;
+};
+
+const findByTestAttr = (wrapper, val) => {
+  return wrapper.find(`[data-test="${val}"]`);
+};
+
+test('renders without error', () => {
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component-app');
+  expect(appComponent.length).toBe(1);
+});
+
+test('renders increment Button', () => {
+  const wrapper = setup();
+  const button = findByTestAttr(wrapper, 'increment-button');
+  expect(button.length).toBe(1);
+});
+
+test('renders counter display', () => {
+  const wrapper = setup();
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.length).toBe(1);
+});
+
+test('counter starts at 0', () => {
+  const wrapper = setup();
+  const initialCounterState = wrapper.state('counter');
+  expect(initialCounterState).toBe(0);
+});
+
+test('clicking button increments counter display', () => {
+  const counter = 7;
+  const wrapper = setup(null, { counter });
+  const button = findByTestAttr(wrapper, 'increment-button');
+  button.simulate('click');
+
+  const counterDisplay = findByTestAttr(wrapper, 'counter-display');
+  expect(counterDisplay.text()).toContain(counter + 1);
 });
